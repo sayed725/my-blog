032735d75064229bdde29842d81f7cd7b48fe31b
sayed725 <ssayed72533@gmail.com>
2025-08-30 12:51:18 +0600

    successfully connect mongodb and inserted json data to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { connectDB } from "@/lib/mongodb";
+import ArticleModel from "@/models/Article";
 
 
 
-export default function Home() {
+
+
+
+export default async function Home() {
+
+ 
+    await connectDB()
+
+    const article = await ArticleModel.find().lean()
+
+    console.log(article)
+
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline mt-20">Hello, from Next.js!</h1>
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,48 +1,46 @@
+
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
+if(!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
 type CachedMongoose = {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-};
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+}
 
-const cached = (globalThis as unknown as { mongooseCache: CachedMongoose })
-  .mongooseCache || { conn: null, promise: null };
+const cached = (globalThis as unknown as { mongooseCache: CachedMongoose }).mongooseCache || {conn: null, promise: null};
 
 export async function connectDB(): Promise<typeof mongoose> {
-  if (cached.conn && cached.conn.connection.readyState === 1) {
-    return cached.conn;
-  }
+    if (cached.conn &&  cached.conn.connection.readyState === 1) {
+        return cached.conn;
+    }
 
-  if (cached.promise) {
+    if(cached.promise) {
+        cached.conn = await cached.promise;
+        return cached.conn;
+    }
+
+    console.log('Establishing a new connection to MongoDB...');
+    cached.promise = mongoose.connect(MONGODB_URI, {
+        bufferCommands: false,
+    }).then((res) => {
+        cached.conn = res;
+        cached.promise = null;
+        return res;
+    }).catch((error) => {
+        console.error("MongoDB connection error:", error);
+        cached.promise = null;
+        throw error;
+    })
+
     cached.conn = await cached.promise;
-    return cached.conn;
-  }
 
-  console.log("Establishing new connection to MongoDB");
+   (globalThis as unknown as { mongooseCache: CachedMongoose }).mongooseCache = cached;
 
-  cached.promise = mongoose
-    .connect(MONGODB_URI, {
-      bufferCommands: false,
-    })
-    .then((res) => {
-      cached.conn = res;
-      cached.promise = null;
-      return res;
-    })
-    .catch((err) => {
-      console.error("Error connecting to MongoDB:", err);
-      cached.promise = null;
-      throw err;
-    });
+   return cached.conn;
 
-  cached.conn = await cached.promise;
-  (globalThis as unknown as { mongooseCache: CachedMongoose }).mongooseCache =
-    cached;
-  return cached.conn;
 }
