4d353c193b88c80692eca5c33c6b22db1b8e17f8
sayed725 <ssayed72533@gmail.com>
2025-09-02 13:01:17 +0600

    done full home section design

--- src/components/ui/BackToTopButton.tsx
+++ src/components/ui/BackToTopButton.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FaChevronUp } from 'react-icons/fa';
 
 const BackToTopButton = () => {
