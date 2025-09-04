7335ec738e5a463d2e14e1c1266157a3edd32017
sayed725 <ssayed72533@gmail.com>
2025-09-04 11:13:33 +0600

    successfull fetch data to blog details page

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,6 +1,43 @@
+import { IArticle } from '@/models/Article';
 import React from 'react'
 
-const ArticleDetailsPage = () => {
+
+async function getArticle(id:string): Promise<IArticle | null> {
+    try {
+        const apiUrl = `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/api/articles/${id}`;
+        const res = await fetch(apiUrl, {next: {revalidate: 100} })
+
+        if(!res.ok){
+          return null
+        }
+
+        const articleData = await res.json()
+
+        return {
+            ...articleData,
+             imageUrl: articleData.imageUrl || 'https://placehold.co/1240x700/e2e8f0/4a5568?text=Featured+Image',
+            authorImageUrl: 'https://placehold.co/120x120/e2e8f0/4a5568?text=Author',
+            comments: articleData.comments
+        }
+
+
+
+    } catch (error) {
+        console.error('Error Fetching article', error)
+        return null
+    }
+}
+
+const ArticleDetailsPage = async({params}: {params: Promise<{id: string}>}) => {
+
+    const {id} = await params;
+
+    const article = await getArticle(id)
+
+    console.log(article)
+
+
+
   return (
     <div>ArticleDetailsPage</div>
   )
